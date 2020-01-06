c660ac4d84470346cc6e6ef9a802d3a2c545c6c8
Marc-Antoine Arnaud <maarnaud@media-io.com>
2020-01-06 16:48:54 +0100

    split env var to retrieve multi paths

--- lib/step_flow/workflow_definitions/workflow_definition.ex
+++ lib/step_flow/workflow_definitions/workflow_definition.ex
@@ -29,12 +29,23 @@ defmodule StepFlow.WorkflowDefinitions.WorkflowDefinition do
 
   def get_workflow_definition_directories do
     case Application.get_env(:step_flow, :workflow_definition) do
-      {:system, key} -> System.get_env(key)
+      {:system, key} ->
+        System.get_env(key)
+        |> String.split(get_separator())
+
       key when is_list(key) -> key
       key when is_bitstring(key) -> [key]
       key ->
         Logger.info("unable to use #{inspect key} to list directory")
         []
+    end
+  end
+
+  defp get_separator do
+    if :os.type() |> elem(0) == :unix do
+      ":"
+    else
+      ";"
     end
   end
 end
