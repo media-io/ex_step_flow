f51ceee96a0cc48b76a997bb6e00f0c3f99b5b82
Valentin NOEL <valentin.noel@media-io.com>
2020-01-14 16:39:30 +0100

    Add get_job_with_status!() function into Jobs module

--- lib/step_flow/jobs/jobs.ex
+++ lib/step_flow/jobs/jobs.ex
@@ -102,6 +102,25 @@ defmodule StepFlow.Jobs do
   def get_job!(id), do: Repo.get!(Job, id)
 
   @doc """
+  Gets a single job with its related status.
+
+  Raises `Ecto.NoResultsError` if the Job does not exist.
+
+  ## Examples
+
+      iex> get_job_with_status!(123)
+      %Job{}
+
+      iex> get_job!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_job_with_status!(id) do
+    get_job!(id)
+    |> Repo.preload(:status)
+  end
+
+  @doc """
   Creates a job.
 
   ## Examples
