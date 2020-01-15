2e5087f8ae050bd2fef915286e479d17149bdf00
Marc-Antoine Arnaud <maarnaud@media-io.com>
2020-01-15 16:50:31 +0100

    bump to 0.0.15

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule StepFlow.MixProject do
   def project do
     [
       app: :step_flow,
-      version: "0.0.14",
+      version: "0.0.15",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
