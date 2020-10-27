afa8195ea9faadc24d9355d79159a831df9e8169
Marc-Antoine Arnaud <maarnaud@media-io.com>
2020-10-27 12:34:45 +0100

    add more unit tests

--- test/workflow_definitions/workflow_definitions_test.exs
+++ test/workflow_definitions/workflow_definitions_test.exs
@@ -14,11 +14,15 @@ defmodule StepFlow.WorkflowDefinitionsTest do
   describe "workflow_definitions" do
     test "list_workflow_definitions/0 returns all workflow_definitions" do
       assert %{
-               data: [%StepFlow.WorkflowDefinitions.WorkflowDefinition{}],
+               data: [workflow],
                page: 0,
                size: 10,
                total: 1
              } = WorkflowDefinitions.list_workflow_definitions()
+
+      assert 5 == length(workflow.steps)
+      assert 3 == length(workflow.start_parameters)
+      assert 1 == length(workflow.parameters)
     end
   end
 end
