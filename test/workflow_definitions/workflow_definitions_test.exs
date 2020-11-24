583602f65ac19367f54040f0ce01fe6be90fd8c5
mat78 <mathauretouze@gmail.com>
2020-11-24 09:42:31 +0100

    ci: fix workflow_definitions_test

--- test/workflow_definitions/workflow_definitions_test.exs
+++ test/workflow_definitions/workflow_definitions_test.exs
@@ -13,10 +13,10 @@ defmodule StepFlow.WorkflowDefinitionsTest do
   describe "workflow_definitions" do
     test "list_workflow_definitions/0 returns all workflow_definitions" do
       assert %{
-               data: [_, workflow, _],
+               data: [workflow, _],
                page: 0,
                size: 10,
-               total: 3
+               total: 2
              } = WorkflowDefinitions.list_workflow_definitions(%{})
 
       assert 5 == length(workflow.steps)
