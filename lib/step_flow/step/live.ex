5bd8d00b14b17ac4448869621457b111bb3d50e8
npierre <nicolas.pierre@francetv.fr>
2020-12-02 14:28:41 +0100

    feat: add step live treatment

---
+++ lib/step_flow/step/live.ex
@@ -0,0 +1,101 @@
+defmodule StepFlow.Step.Live do
+  @moduledoc """
+  The Live step context.
+  """
+  alias StepFlow.Amqp.CommonEmitter
+  alias StepFlow.Jobs
+  alias StepFlow.Jobs.Status
+  alias StepFlow.Repo
+  alias StepFlow.Step.Launch
+  alias StepFlow.Step.LaunchParams
+
+  def update_job_live(
+        [source_path | _source_paths],
+        launch_params,
+        workflow,
+        step
+      ) do
+    workflow_id = workflow.id
+    step_id = StepFlow.Map.get_by_key_or_atom(step, :id)
+
+    Jobs.list_jobs(%{
+      step_id: step_id,
+      workflow_id: workflow_id
+    })
+
+    case Jobs.get_by(%{"workflow_id" => workflow_id, "step_id" => step_id}) do
+      nil ->
+        create_live_worker(source_path, launch_params)
+
+      job ->
+        job = Repo.preload(job, :status)
+
+        case Status.get_last_status(job.status).state do
+          :ready_to_init -> update_live_worker(launch_params, job)
+          :ready_to_start -> update_live_worker(launch_params, job)
+          :completed -> delete_live_worker(launch_params, job)
+          _ -> {:ok, "nothing to do"}
+        end
+    end
+  end
+
+  defp create_live_worker(source_path, launch_params) do
+    message = Launch.generate_message_one_for_one(source_path, launch_params)
+
+    message =
+      Map.put(
+        message,
+        :parameters,
+        message.parameters ++ [%{id: "action", type: "string", value: "create"}]
+      )
+
+    case CommonEmitter.publish_json(
+           "job_worker_manager",
+           LaunchParams.get_step_id(launch_params),
+           message
+         ) do
+      :ok -> {:ok, "started"}
+      _ -> {:error, "unable to publish message"}
+    end
+  end
+
+  defp update_live_worker(launch_params, job) do
+    generate_message(job)
+    |> publish_message(launch_params)
+  end
+
+  defp delete_live_worker(launch_params, job) do
+    message = generate_message(job)
+
+    case CommonEmitter.publish_json(
+           "job_worker_manager",
+           LaunchParams.get_step_id(launch_params),
+           message
+         ) do
+      :ok -> {:ok, "deleted"}
+      _ -> {:error, "unable to publish message"}
+    end
+  end
+
+  def generate_message(job) do
+    message = Jobs.get_message(job)
+
+    action_parameter =
+      job.status
+      |> Status.get_last_status()
+      |> Status.get_action_parameter()
+
+    Map.put(message, :parameters, message.parameters ++ action_parameter)
+  end
+
+  defp publish_message(message, launch_params) do
+    case CommonEmitter.publish_json(
+           LaunchParams.get_step_parameter(launch_params, "direct_messaging_queue").value,
+           LaunchParams.get_step_id(launch_params),
+           message
+         ) do
+      :ok -> {:ok, "started"}
+      _ -> {:error, "unable to publish message"}
+    end
+  end
+end
