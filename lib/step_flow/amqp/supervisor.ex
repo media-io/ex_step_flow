8a0a6aea8ec93f0cb791ad993868fcebcb28a9c9
npierre <nicolas.pierre@francetv.fr>
2020-12-13 10:12:04 +0100

    wip

--- lib/step_flow/amqp/supervisor.ex
+++ lib/step_flow/amqp/supervisor.ex
@@ -24,6 +24,7 @@ defmodule StepFlow.Amqp.Supervisor do
       worker(StepFlow.Amqp.CompletedConsumer, []),
       worker(StepFlow.Amqp.ErrorConsumer, []),
       worker(StepFlow.Amqp.ProgressionConsumer, []),
+      worker(StepFlow.Amqp.QueueNotFoundConsumer, []),
       worker(StepFlow.Amqp.WorkerDiscoveryConsumer, [])
     ]
 
